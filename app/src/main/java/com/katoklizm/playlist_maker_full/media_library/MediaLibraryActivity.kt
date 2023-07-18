7307c15c10f7df37d046b833867533549cf57534
Demonided <Demonided@mail.ru>
2023-07-18 14:07:56 +0300

    new commit, new project

---
+++ app/src/main/java/com/katoklizm/playlist_maker_full/media_library/MediaLibraryActivity.kt
@@ -0,0 +1,12 @@
+package com.katoklizm.playlist_maker_full.media_library
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.katoklizm.playlist_maker_full.R
+
+class MediaLibraryActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_media_library)
+    }
+}
---
+++ app/src/main/java/com/katoklizm/playlist_maker_full/search/SearchActivity.kt
@@ -0,0 +1,12 @@
+package com.katoklizm.playlist_maker_full.search
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.katoklizm.playlist_maker_full.R
+
+class SearchActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_search)
+    }
+}
