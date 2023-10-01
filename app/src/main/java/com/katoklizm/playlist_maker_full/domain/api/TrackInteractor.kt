0f4285c070ad202df7c0f48eb8b55224cd2309a4
demonided <demonided@mail.ru>
2023-10-01 20:25:57 +0300

    mnnas

--- app/src/main/java/com/katoklizm/playlist_maker_full/domain/api/TrackInteractor.kt
+++ app/src/main/java/com/katoklizm/playlist_maker_full/domain/api/TrackInteractor.kt
@@ -3,9 +3,10 @@ package com.katoklizm.playlist_maker_full.domain.api
 import com.katoklizm.playlist_maker_full.domain.model.Track
 
 interface TrackInteractor {
+
     fun searchTrack(term: String, consumer: TrackConsumer)
 
     interface TrackConsumer{
-        fun consume(foundTrack: List<Track>)
+        fun consume(foundTrack: List<Track>?, errorMessage: String?)
     }
 }
