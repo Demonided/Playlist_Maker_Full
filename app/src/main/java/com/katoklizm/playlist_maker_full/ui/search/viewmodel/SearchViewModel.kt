8352c4af0a81805226d986d4980fab030eaed7d7
demonided <demonided@mail.ru>
2023-10-22 21:57:19 +0300

    update SearchViewModel

--- app/src/main/java/com/katoklizm/playlist_maker_full/ui/search/viewmodel/SearchViewModel.kt
+++ app/src/main/java/com/katoklizm/playlist_maker_full/ui/search/viewmodel/SearchViewModel.kt
@@ -1,35 +1,29 @@
 package com.katoklizm.playlist_maker_full.ui.search.viewmodel
 
-import android.app.Application
-import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
-import com.katoklizm.playlist_maker_full.R
-import com.katoklizm.playlist_maker_full.util.Creator
 import com.katoklizm.playlist_maker_full.domain.search.api.TrackInteractor
 import com.katoklizm.playlist_maker_full.domain.search.model.Track
 import com.katoklizm.playlist_maker_full.domain.search.SearchState
 
 class SearchViewModel(
-    application: Application
-) : AndroidViewModel(application) {
+    private val trackInteractor: TrackInteractor
+) : ViewModel() {
 
     companion object {
         fun getModelFactory(): ViewModelProvider.Factory = viewModelFactory {
             initializer {
-                SearchViewModel(this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as Application)
+                SearchViewModel(this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as TrackInteractor)
             }
         }
     }
 
-    private val trackInteractor = Creator.provideTrackInteractor(getApplication())
-
     private val stateLiveData = MutableLiveData<SearchState>()
 
-    val emptyList = getApplication<Application>().getString(R.string.empty_list)
     fun observeState(): LiveData<SearchState> = stateLiveData
 
     private fun renderState(state: SearchState) {
@@ -82,7 +76,7 @@ class SearchViewModel(
                             if (foundTrack.isNotEmpty()) {
                                 renderState(SearchState.ContentListSearchTrack(foundTrack))
                             } else {
-                                renderState(SearchState.Empty(emptyList))
+                                renderState(SearchState.Empty(""))
                             }
                         }
 
